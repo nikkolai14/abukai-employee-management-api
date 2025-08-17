6f23a73c2154a22cb3b9cabdaafb153538168ad3
nikkolai14 <nikkolaifernandez14@gmail.com>
2025-08-18 00:11:33 +0800

    Make validate method public and remove unused integer rule check

--- app/Core/Validator.php
+++ app/Core/Validator.php
@@ -6,7 +6,7 @@ class Validator
 {
     protected $errors = [];
 
-    protected function validate(
+    public function validate(
         array $data = [],
         array $rules = []
     ) {
@@ -26,15 +26,11 @@ class Validator
             $this->errors[$field][] = "$field must be a string.";
         }
 
-        if (strpos($rule, 'integer') !== false && !is_int($value)) {
-            $this->errors[$field][] = "$field must be an integer.";
-        }
-
         if (strpos($rule, 'email') !== false && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
             $this->errors[$field][] = "$field must be a valid email address.";
         }
 
-        if (strpos($rule, 'position_number') !== false && (!is_numeric($value) || $value <= 0)) {
+        if (strpos($rule, 'positive_number') !== false && (!is_numeric($value) || $value <= 0)) {
             $this->errors[$field][] = "$field must be a positive number.";
         }
     }
